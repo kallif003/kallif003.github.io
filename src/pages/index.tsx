a4b9ae5612ba8e810d97b18a654bb28b170e0d61
kallif003 <kallifabrahao@gmail.com>
2021-12-16 13:09:10 -0300

    teste-husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 </Head>
 
-			    <main className={styles.main}>
+			        <main className={styles.main}>
 				<h1 className={styles.title}>
 					Welcome to <a href="https://nextjs.org">Next.js!</a>
 				</h1>
