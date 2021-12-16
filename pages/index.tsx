6de0ccd100f96ebf1a2b148a9b86a86e39b74eca
kallif003 <kallifabrahao@gmail.com>
2021-12-16 09:47:22 -0300

    teste-lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 
+const =e dk
+
 const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
