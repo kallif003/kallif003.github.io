892df4fca455fae1e49583fef822b4244d127714
kallif003 <kallifabrahao@gmail.com>
2021-12-16 09:40:37 -0300

    teste-husky-lint-staged

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports =
+{
+    "printWidth": 100,
+    "tabWidth": 2,
+    "useTabs": true,
+    "semi": false,
+    "singleQuote": false,
+    "trailingComma": "es5",
+    "bracketSpacing": true,
+    "jsxBracketSameLine": false,
+    "jsxSingleQuote": false,
+    "bracketSameLine": true
+  }
