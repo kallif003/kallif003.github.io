ee1edf32a9e3ff5bc3e33550c8560cf9c8b2a853
kallif003 <kallifabrahao@gmail.com>
2021-12-16 10:58:12 -0300

    atualizacao

--- .prettierrc.js
+++ .prettierrc.js
@@ -9,5 +9,6 @@ module.exports =
     "bracketSpacing": true,
     "jsxBracketSameLine": false,
     "jsxSingleQuote": false,
-    "bracketSameLine": true
+    "bracketSameLine": true,
+    
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 
-const teste = teste
-
 const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
