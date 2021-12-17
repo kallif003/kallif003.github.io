7a3a40f97ebc1ef20843cf4f443a9d609bedff64
kallif003 <kallifabrahao@gmail.com>
2021-12-17 12:24:31 -0300

    validado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next"
 import React from "react"
 
 const Home: NextPage = function () {
-	return <h3 className="text-6xl text-white">Hello world!</h3>
+	return <   h3 className="text-6xl text-white">Hello world!</h3>
 }
 
 export default Home
