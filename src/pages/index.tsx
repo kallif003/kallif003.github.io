35e4df25d5babe54ccd8a166c78e124c4b353b15
kallif003 <kallifabrahao@gmail.com>
2021-12-17 12:54:22 -0300

    correto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next"
 import React from "react"
 
+const teste = "teste"
+console.log(teste)
 const Home: NextPage = function () {
 	return <h3 className="text-6xl text-white">Hello world!</h3>
 }
