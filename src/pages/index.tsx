44edaaf38a3fc2beceaee15dba6f266b93a6ec42
Felipe Pereira <fe@lipe.dev>
2021-12-17 17:36:37 -0300

    change site title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next"
 import React from "react"
+import Head from "next/head"
 
 const teste = "teste"
 console.log(teste)
 const Home: NextPage = function () {
-	return <h3 className="text-6xl text-white">Hello world!</h3>
+	return (
+		<div>
+			<Head>
+				<title>Site do Kallif</title>
+			</Head>
+			<h3 className="text-6xl text-white">Hello world!</h3>
+		</div>
+	)
 }
 
 export default Home
