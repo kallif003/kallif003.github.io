44bca780c017e878d79e486d4bbf9265df387311
kallif003 <kallifabrahao@gmail.com>
2021-12-20 12:14:33 -0300

    chore: adicao commitlint e commitizen
    foi adicionado ao projeto o commitlint e o commitizen para padronizaçao dos commits

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+<<<<<<< HEAD
 	"printWidth": 80,
 	"tabWidth": 2,
 	"useTabs": true,
@@ -8,4 +9,15 @@ module.exports = {
 	"bracketSpacing": true,
 	"jsxSingleQuote": false,
 	"bracketSameLine": true,
+=======
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: false,
+	trailingComma: "es5",
+	bracketSpacing: true,
+	jsxSingleQuote: false,
+	bracketSameLine: true,
+>>>>>>> chore: adicao commitlint e commitizen
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
 import type { NextPage } from "next"
+<<<<<<< HEAD
 import React, { useState } from "react"
+=======
+import React from "react"
+>>>>>>> chore: adicao commitlint e commitizen
 import Head from "next/head"
 
 const Home: NextPage = function () {
+<<<<<<< HEAD
 	const [count, setCount] = useState<number>(0)
 
+=======
+>>>>>>> chore: adicao commitlint e commitizen
 	return (
 		<div>
 			<Head>
 				<title>Kallif - Dev</title>
 			</Head>
 			<h3 className="text-6xl text-white">Hello world!</h3>
+<<<<<<< HEAD
 			<p>Bem vindo ao site do Kallif</p>
 			<button
 				onClick={() => {
@@ -19,6 +27,8 @@ const Home: NextPage = function () {
 				Count +
 			</button>
 			<p>Count is {count}</p>
+=======
+>>>>>>> chore: adicao commitlint e commitizen
 		</div>
 	)
 }
