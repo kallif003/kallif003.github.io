c9fe6b6fcddde338fe77fd11064cb93fb97d8c9b
Felipe Pereira <fe@lipe.dev>
2022-01-04 17:33:00 -0300

    chore(project): resolve merge conflicts

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-	"printWidth": 80,
-	"tabWidth": 2,
-	"useTabs": true,
-	"semi": false,
-	"singleQuote": false,
-	"trailingComma": "es5",
-	"bracketSpacing": true,
-	"jsxSingleQuote": false,
-	"bracketSameLine": true,
-=======
 	printWidth: 80,
 	tabWidth: 2,
 	useTabs: true,
@@ -19,5 +8,4 @@ module.exports = {
 	bracketSpacing: true,
 	jsxSingleQuote: false,
 	bracketSameLine: true,
->>>>>>> chore: adicao commitlint e commitizen
 }
