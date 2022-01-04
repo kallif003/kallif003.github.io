12d9386453e7acccda81c909a4a7852582408956
Felipe Pereira <fe@lipe.dev>
2022-01-04 17:33:00 -0300

    chore(project): resolve merge conflicts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,16 @@
 import type { NextPage } from "next"
-<<<<<<< HEAD
 import React, { useState } from "react"
-=======
-import React from "react"
->>>>>>> chore: adicao commitlint e commitizen
 import Head from "next/head"
 
 const Home: NextPage = function () {
-<<<<<<< HEAD
 	const [count, setCount] = useState<number>(0)
 
-=======
->>>>>>> chore: adicao commitlint e commitizen
 	return (
 		<div>
-			  <Head>
+			<Head>
 				<title>Kallif - Dev</title>
 			</Head>
 			<h3 className="text-6xl text-white">Hello world!</h3>
-<<<<<<< HEAD
 			<p>Bem vindo ao site do Kallif</p>
 			<button
 				onClick={() => {
@@ -27,8 +19,6 @@ const Home: NextPage = function () {
 				Count +
 			</button>
 			<p>Count is {count}</p>
-=======
->>>>>>> chore: adicao commitlint e commitizen
 		</div>
 	)
 }
