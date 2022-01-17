98be37560385ff4f5683cd19fc3f0a27dd3314fc
kallif003 <kallifabrahao@gmail.com>
2022-01-17 12:33:21 -0300

    feat(Button)test and storybook changes

--- src/components/Buttons/Buttons.test.tsx
+++ src/components/Buttons/Buttons.test.tsx
@@ -10,6 +10,9 @@ import {
 	Button6,
 	Button7,
 	Button8,
+	Ul,
+	Li,
+	Ancora,
 } from "."
 
 describe("Buttons", () => {
@@ -43,6 +46,18 @@ describe("Buttons", () => {
 	})
 	it("renders Button8 properly", () => {
 		const { container } = render(<Button8>Teste</Button8>)
+		expect(container.firstChild).toMatchSnapshot()
+	})
+	it("renders Ul properly", () => {
+		const { container } = render(<Ul>Teste</Ul>)
+		expect(container.firstChild).toMatchSnapshot()
+	})
+	it("renders Li properly", () => {
+		const { container } = render(<Li>Teste</Li>)
+		expect(container.firstChild).toMatchSnapshot()
+	})
+	it("renders Ancora properly", () => {
+		const { container } = render(<Ancora>Teste</Ancora>)
 		expect(container.firstChild).toMatchSnapshot()
 	})
 })
