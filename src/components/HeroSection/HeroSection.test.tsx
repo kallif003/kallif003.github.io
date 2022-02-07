fc830c0b2c8b26bb4f83224d135b5c3aa8bebbff
kallif003 <kallifabrahao@gmail.com>
2022-02-07 14:14:10 -0300

    feat(HeroSectionComponents)new changes in Hero Section Components

--- src/components/HeroSection/HeroSection.test.tsx
+++ src/components/HeroSection/HeroSection.test.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { render } from "@testing-library/react"
-import { HeroSection, Container, Imagem } from "../HeroSection/HeroSection"
-import img from "../../../assets/Image/sistema-delivery.png"
+import { HeroSection, Container } from "../HeroSection/HeroSection"
 
 describe("Hero Section Components", () => {
 	it("renders HeroSection properly", () => {
@@ -11,8 +10,5 @@ describe("Hero Section Components", () => {
 	it("renders Container properly", () => {
 		const { container } = render(<Container>Teste</Container>)
 		expect(container.firstChild).toMatchSnapshot()
-	})
-	it("renders Image properly", () => {
-		render(<Imagem src={img} alt="Sistema Delivery" />)
 	})
 })
