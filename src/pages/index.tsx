382c9ad9551ca52711b3a326eeddd6d9e1ad9d95
kallif003 <kallifabrahao@gmail.com>
2022-03-31 16:53:31 -0300

    feat: :sparkles: create open graph

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,13 @@ const Home: NextPage = function () {
 		<div>
 			<Head>
 				<title>Kallif - Dev</title>
+				<meta property="og:title" content="Kallif - Dev. FrontEnd" />
+				<meta property="og:url" content="https://kallif003.github.io/" />
+				<meta property="og:type" content="website" />
+				<meta
+					property="og:image"
+					content="../../../assests/Image/portifolio.png"
+				/>
 			</Head>
 			<meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
 			<link rel="icon" href="/favicon.ico" />
