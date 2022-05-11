5e6b7f2d9dd21bd93a832a847d31545a3e96747c
kallif003 <kallif003@users.noreply.github.com>
2022-05-11 14:50:27 +0000

    Deploying to gh-pages from @ kallif003/kallif003.github.io@a1a6b579e60fda8c4f48d298609d6d8167221a22 🚀

---
+++ _next/static/chunks/pages/AboutMe/AboutMe-42df96d7dfa372cb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[52],{606:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/AboutMe/AboutMe",function(){return _(7107)}])}},function(n){n.O(0,[702,919,790,993,774,888,179],(function(){return u=606,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
--- _next/static/chunks/pages/HomePage/HomePage-1c1d11bb25c59eed.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[739],{8284:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/HomePage/HomePage",function(){return e(5191)}])}},function(n){n.O(0,[702,847,518,249,790,191,774,888,179],(function(){return _=8284,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
