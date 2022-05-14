8d975f6d4796c55a0cde618255538d53c64543f5
kallif003 <kallifabrahao@gmail.com>
2022-05-14 18:06:55 -0300

    feat: :sparkles: post-file changes

--- src/pages/MyBlog/post/index.tsx
+++ src/pages/MyBlog/post/index.tsx
@@ -17,13 +17,13 @@ interface List {
 export default function MyBlog() {
 	const router = useRouter()
 	const [post, setPost] = useState<List[]>([])
-
 	const { slug } = router.query
 
 	useEffect(() => {
 		async function getProps() {
 			const client = createClient()
 			const response = [await client.getByUID("my-blog", String(slug))]
+
 			const data = response.map((p) => ({
 				id: p.id,
 				title: p.data.title,
@@ -40,25 +40,46 @@ export default function MyBlog() {
 		}
 	})
 	return (
-		<section className="w-screen">
+		<section className="">
 			{post.map((p) => (
 				<div
 					key={p.id}
-					className="my-10  text-center flex flex-col items-center text-white">
+					className="flex flex-col items-center text-center text-white mx-5 my-5">
 					<H1Head className="text-yellow sm:text-[30px] sm:leading-9 pb-2">
 						{p.title}
 					</H1Head>
 					<h2 className="pb-5">{p.description}</h2>
-					<Imagem
-						src={p.image}
-						alt={p.title}
-						height={415}
-						width={800}
-						quality={100}
-						className="opacity-20 "
-					/>
-					<div className=" w-[50rem] mt-5 text-left sm:w-[18rem] bg-slate-500">
-						<PrismicRichText field={p.richText} />
+					<div className="sm:w-[20rem] md:w-[40rem]">
+						<Imagem
+							src={p.image}
+							alt={p.title}
+							height={415}
+							width={800}
+							quality={100}
+							className="opacity-20 "
+						/>
+					</div>
+
+					<div className=" text-white text-left my-5 mx-20 w-[50rem] sm:w-[20rem] md:w-[40rem]">
+						<PrismicRichText
+							field={p.richText}
+							components={{
+								heading1: ({ children }) => (
+									<h1 className="mt-4 text-yellow text-[20px]">{children}</h1>
+								),
+								heading2: ({ children }) => (
+									<h1 className="my-4 font-bold text-[15px]">{children}</h1>
+								),
+								heading3: ({ children }) => (
+									<h1 className="text-yellow font-bold text-[15px] mt-4">
+										{children}
+									</h1>
+								),
+								paragraph: ({ children }) => (
+									<p className="paragraph ">{children}</p>
+								),
+							}}
+						/>
 					</div>
 				</div>
 			))}
